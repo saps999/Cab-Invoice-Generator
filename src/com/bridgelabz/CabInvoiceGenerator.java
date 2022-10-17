ad3a77b1e06c4b8187d5bdb45e25024a877d1cc6
saps999 <saptarshibiswasgang@gmail.com>
2022-10-18 00:59:00 +0530

    Multiplr rides

--- src/com/bridgelabz/CabInvoiceGenerator.java
+++ src/com/bridgelabz/CabInvoiceGenerator.java
@@ -2,15 +2,13 @@ package com.bridgelabz;
 
 public class CabInvoiceGenerator {
     /**
-     *   variable
+     * variable
      */
     private static final int COST_PER_KM = 10;
     private static final int COST_PER_MIN = 1;
     private static final int MIN_FARE = 5;
+
     public double CalculateFare(double distance, int time) {
-        /**
-         * formula for total fare
-         */
         double totalFare = distance * COST_PER_KM + time * COST_PER_MIN;
         if (totalFare < MIN_FARE) {
             return MIN_FARE;
@@ -18,16 +16,21 @@ public class CabInvoiceGenerator {
         return totalFare;
     }
 
-    public static void main(String[] args) {
+    public double calculateFareForMultipleRides(Ride[] rides) {
+        // variable
+        double totalFare = 0.0;
         /**
-         * create object for CabInvoiceGenerator class, object name is cabInvoiceGenerator
+         * using for each loop
          */
-        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
-        /**
-         * calling CalculateFare method from cabInvoiceGenerator object
-         * and print o/p
-         */
-        System.out.println(cabInvoiceGenerator.CalculateFare(5, 20) + " Rs");
+        for (Ride ride : rides) {
+            totalFare = CalculateFare(ride.getDistance(), ride.getTime());
+        }
+        return totalFare;
     }
 
+    public static void main(String[] args) {
+
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+        System.out.println(cabInvoiceGenerator.CalculateFare(5, 20) + " Rs");
+    }
 }
--- src/com/bridgelabz/InvoiceServiceTest.java
+++ src/com/bridgelabz/InvoiceServiceTest.java
@@ -7,19 +7,31 @@ public class InvoiceServiceTest {
 
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
-        CabInvoiceGenerator invoiceService = new  CabInvoiceGenerator();
+        CabInvoiceGenerator invoiceService = new CabInvoiceGenerator();
         double distance = 2.0;
         int time = 5;
         double totalFare = invoiceService.CalculateFare(distance, time);
-        Assert.assertEquals(25, totalFare,0);
+        Assert.assertEquals(25, totalFare, 0);
     }
 
     @Test
     public void givenLessDistanceAndTime_ShouldReturnMinFare() {
-        CabInvoiceGenerator invoiceService = new  CabInvoiceGenerator();
+        CabInvoiceGenerator invoiceService = new CabInvoiceGenerator();
         double distance = 0.1;
         int time = 1;
         double totalFare = invoiceService.CalculateFare(distance, time);
-        Assert.assertEquals(5.0, totalFare,0);
+        Assert.assertEquals(5.0, totalFare, 0); //UC1
+    }
+
+    @Test
+    public void givenMultipleRides_ShouldReturnTotalOfTotalFare() {
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(5.0, 10),
+                new Ride(0.1, 1),
+                new Ride(20, 60)
+        };
+        CabInvoiceGenerator invoiceService = new CabInvoiceGenerator();
+        double totalFare = invoiceService.calculateFareForMultipleRides(rides);
+        Assert.assertEquals(260, totalFare, 0);//UC2
     }
 }
